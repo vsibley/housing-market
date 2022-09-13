4b478db0eefa358e10b4abf77f1512d8cbe7dd61
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-09-13 16:20:58 -0700

    added spinner & PrivateRouteshook to  Profile page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoutes";
 import ForgotPassword from "../src/pages/ForgotPassword";
 import Offers from "../src/pages/Offers";
 import SignUp from "../src/pages/SignUp";
@@ -16,7 +17,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
