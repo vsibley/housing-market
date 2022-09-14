0f7799b1f030194f6a621b99b936e164664b09d4
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-09-14 14:16:37 -0700

    Added Category page and created ListingItem component. Also created offers page.

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -4,6 +4,7 @@ import { collection, getDocs, query, where, orderBy, limit, startAfter } from "f
 import { toast } from 'react-toastify';
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItem';
 
 
 
@@ -53,7 +54,7 @@ function Category() {
 
 
   return (
-    <div className='category'>Category misc
+    <div className='category'>Category
       <header>
         <p className='pageHeader'>
           {params.categoryName === 'rent' ? 'Places for Rent' : 'Places for Sale'}
@@ -66,7 +67,7 @@ function Category() {
         <main>
           <ul className="categoryListings">
             {listings.map((listing) => (
-              <h3 key={listing.id}>{listing.data.name}</h3>
+              <ListingItem listing={listing.data} id={listing.id} key={listing.id}/>
             ))}
           </ul>
         </main>
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,8 +1,81 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { collection, getDocs, query, where, orderBy, limit, startAfter } from "firebase/firestore";
+import { toast } from 'react-toastify';
+import { db } from '../firebase.config'
+import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItem';
+
+
 
 function Offers() {
+
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true)
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        //Get reference 
+        const listingsRef = collection(db, 'listings')
+
+        //create a querty 
+        const q = query(
+          listingsRef,
+          where('offer', '==', true),
+          orderBy('timestamp', 'desc'),
+          limit(10),
+        )
+
+        //Excecute query 
+        const querySnap = await getDocs(q)
+
+        let listings = []
+
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data()
+          })
+        })
+
+        setListings(listings)
+        setLoading(false)
+      } catch (error) {
+        toast.error('Could not fetch listing, please try again.')
+      }
+    }
+
+    fetchListings()
+  }, );
+
+
+
   return (
-    <h1>Offers</h1>
+    <div className='category'>
+      <header>
+        <p className='pageHeader'>
+         Offers
+        </p>
+      </header>
+      {loading ? (<Spinner />
+      ) : listings && listings.length > 0 ? (
+
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem listing={listing.data} id={listing.id} key={listing.id} />
+              ))}
+            </ul>
+          </main>
+        </>
+
+      ) : (<p>There are no offers currently available.</p>
+      )}
+    </div>
   )
 }
 
