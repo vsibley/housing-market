a431104f3f2d716ac1ceb48a757c3f633a12d2f9
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-09-15 08:05:39 -0700

    fixed geolocation address error

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -85,10 +85,16 @@ function CreateListing() {
             const data = await response.json()
             geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
             geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+
+
+
             location =
                 data.status === 'ZERO_RESULTS'
                     ? undefined
                     : data.results[0]?.formatted_address
+
+
+
             if (location === undefined || location.includes('undefined')) {
                 setLoading(false)
                 toast.error('Please enter a correct address')
@@ -97,7 +103,7 @@ function CreateListing() {
         } else {
             geolocation.lat = latitude
             geolocation.lng = longitude
-            location = address
+            
         }
         // Store image in firebase
         const storeImage = async (image) => {
@@ -149,9 +155,9 @@ function CreateListing() {
             timestamp: serverTimestamp(),
         }
 
+        formDataCopy.location = address
         delete formDataCopy.images
         delete formDataCopy.address
-        location && (formDataCopy.location = location)
         !formDataCopy.offer && delete formDataCopy.discountedPrice
 
         const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
