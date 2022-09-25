14c6fea75e3a69dca161749d757d96611539ed6a
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-09-25 09:49:00 -0700

    fixed css issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  },
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
